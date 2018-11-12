666f94c0bc621d820b763b24227def2548b424b3
Unknown <rodry.sauco@gmail.com>
2018-11-11 22:16:13 -0300

    Improving v9

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -55,7 +55,7 @@
             <tr>
               <td><span>Desviación estandar</span></td> <!-- Desviacion estandar -->
               <td>
-                <el-input-number v-model="randDrink.desviacionEstandar" size="mini" :step='0.01' :min=0></el-input-number>
+                <el-input-number v-model="randDrink.desviacionEstandar" size="mini" :min=0></el-input-number>
               </td>
             </tr>
             <tr>
@@ -71,14 +71,14 @@
               </td>
             </tr>
           </table>
+          <div class="modelsHeader">
+            <el-switch v-model="isPSelected" active-text="Usar modelo P" inactive-text=" Usar modelo Q"></el-switch>
+          </div>
           <div class="continue">
             <el-button type="primary" :disabled="!butt" @click="go1plus"><i class="el-icon-arrow-right"></i> Continuar</el-button>
           </div>
         </div>
         <div v-if="active == 1">
-          <div class="modelsHeader">
-            <el-switch v-model="isPSelected" active-text="Usar modelo P" inactive-text=" Usar modelo Q"></el-switch>
-          </div>
           <div>
             <div v-if="isPSelected">
               <table class="tableCalculos">
@@ -97,11 +97,12 @@
               </table>
             </div>
             <div v-else>
+              <p>Busque en la tabla Brown: {{modeloQ.zD}}</p>
               <table class="tableCalculos">
                 <tr>
                   <td>Desviación demanda</td>
                   <td>
-                    <el-input-number v-model="modeloQ.oD" size="mini" :step="0.01" :min=0></el-input-number>
+                    <el-input-number v-model="modeloQ.eZ" size="mini" :step="0.01"></el-input-number>
                   </td>
                 </tr>
               </table>
@@ -115,163 +116,206 @@
           </div>
         </div>
         <div v-if="active > 1">
-          muestra los results
+          <section>
+            <div>Costo Total Esperado: {{modeloQ.CTE}}</div>
+            <div>Cantidad optima de compra: {{resultado.cantidadOptima}}</div>
+            <div>Punto de reorden: {{resultado.reorden}}</div>
+            <div>faltantes: {{resultado.faltantes}}</div>
+            <div>Nº de Ordenes de compra anuales:{{resultado.nOrdenes}}</div>
+          </section>
         </div>
       </div>
     </el-card>
   </div>
 </template>
 <script>
-  export default {
-    name: 'home-component',
-    data() {
-      return {
-        active: 0,
-        isPSelected: true,
-        randDrink: {
-          costo: 0, //C
-          costoAlma: 0, //H
-          costoPedido: 0, //S
-          demandaAnual: 0, //D
-          demandaDiaria: 0, //d
-          leadTime: 0, //L
-          desviacionEstandar: 0, //Z
-          nivelServicio: 0, //P
-          diasLaborales: 0
-        },
-        modeloP: {
-          sigmaL: 0, //desviacion durante entrega
-          revision: 0, //T
-          inventario: 0, //I
-        },
-        modeloQ: {
-          oD: 0, //Desviacion de la demanda
-          oL: 0, // Desviación estándar de la demanda durante el plazo de entrega
-          CTE: 0, //CTE
-        },
-        resultado: {
-          reorden: 0, //R          
-          cantidadOptima: 0, //Q optima
-          faltantes: 0, //E(z)
-        }
+export default {
+  name: "home-component",
+  data() {
+    return {
+      active: 1,
+      isPSelected: false,
+      randDrink: {
+        costo: 5000, //C
+        costoAlma: 60, //H
+        costoPedido: 160, //S
+        demandaAnual: 0, //D
+        demandaDiaria: 400, //d
+        leadTime: 9, //L
+        desviacionEstandar: 5, //Z
+        nivelServicio: 98, //P
+        diasLaborales: 25
+      },
+      modeloP: {
+        sigmaL: 0, //desviacion durante entrega
+        revision: 0, //T
+        inventario: 0 //I
+      },
+      modeloQ: {
+        zD: 0, //Valor de la tabla brown
+        eZ: 0, // Pide el valor de la tabla
+        oL: 0, // Desviación estándar de la demanda durante el plazo de entrega
+        CTE: 0 //CTE
+      },
+      resultado: {
+        nOrdenes:0, //D/q
+        reorden: 0, //R
+        cantidadOptima: 0, //Q optima
+        faltantes: 0 //E(z)
+      }
+    };
+  },
+  computed: {
+    d: function() {
+      if (this.randDrink.demandaAnual !== 0) {
+        return true;
+      } else {
+        return false;
       }
     },
-    computed: {
-      d: function () {
-        if (this.randDrink.demandaAnual !== 0) {
-          return true;
-        } else {
-          return false;
-        }
-      },
-      D: function () {
-        if (this.randDrink.demandaDiaria !== 0) {
-          return true;
-        } else {
-          return false;
-        }
-      },
-      butt: function () {
-        if ((this.randDrink.demandaDiaria || this.randDrink.demandaAnual) && this.randDrink.costo && this.randDrink.costoAlma &&
-          this.randDrink.costoPedido && this.randDrink.leadTime && this.randDrink.desviacionEstandar && this.randDrink
-          .nivelServicio) {
-          return true;
-        } else {
-          return false;
-        }
+    D: function() {
+      if (this.randDrink.demandaDiaria !== 0) {
+        return true;
+      } else {
+        return false;
       }
     },
-    methods: {
-      goBack() {
-        this.active--;
-      },
-      go1plus() {
-        if (this.active < 2) {
-          this.active++;
-        } else {
-          this.active = 0;
-        }
-        if (this.active === 2) {
-          this.randDrink.nivelServicio = this.randDrink.nivelServicio / 100;
-          if (this.randDrink.demandaAnual === 0){
-            this.randDrink.demandaAnual = (this.demandaDiaria * this.randDrink.diasLaborales) * 12;
-          }
-          else {
-            this.randDrink.demandaDiaria = (this.demandaAnual / 12) / this.randDrink.diasLaborales;
-          }
-          if (this.isPSelected) {
-            this.calcularSigmaL();
-            this.calcularQ();
-          } else {            
-            this.calcularQ();
-            this.calcularoL();
-            this.calcularReorden();
-            this.calcularCTE();
-          }
-        }
-      },
-      calcularoL(){
-        this.modeloQ.oL = Math.sqrt(this.randDrink.leadTime) * this.modeloQ.oD;
-      },
-      calcularReorden(){
-        let rta = (this.randDrink.demandaDiaria * this.randDrink.leadTime) + (this.randDrink.desviacionEstandar + this.modeloQ.oL);
-        if (rta < 1) {
-          rta = 1;
-        }
-        this.resultado.reorden = rta;        
-      },
-      calcularCTE() {
-        let cte = (this.randDrink.demandaAnual * this.randDrink.costo) + ((this.randDrink.demandaAnual / this.resultado.cantidadOptima) *
-          this.randDrink.costoPedido) + ((this.resultado.cantidadOptima / 2) * this.randDrink.costoAlma);
-        this.modeloQ.CTE = Math.round(cte * 1000) / 1000;
-      },      
-      calculoFaltantes() {
-
-      },
-      calcularQ() {
+    butt: function() {
+      if (
+        (this.randDrink.demandaDiaria || this.randDrink.demandaAnual) &&
+        this.randDrink.costo &&
+        this.randDrink.costoAlma &&
+        this.randDrink.costoPedido &&
+        this.randDrink.leadTime &&
+        this.randDrink.desviacionEstandar &&
+        this.randDrink.nivelServicio
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  methods: {
+    goBack() {
+      this.active--;
+    },
+    go1plus() {
+      if (this.active < 2) {
         if (this.isPSelected) {
-          let rta = (this.randDrink.demandaDiaria * (this.modeloP.revision + this.randDrink.leadTime)) + (this.randDrink.desviacionEstandar *
-            this.modeloP.sigmaL) - this.modeloP.inventario;
-          if (rta < 0) {
-            rta = 0;
+          this.active = 2;
+        } else {
+          this.active++;
+          if (this.active === 1) {
+            this.preparar();
           }
-          this.resultado.cantidadOptima = Math.floor(rta);
-        } else { //si es q
-          let cpo = Math.sqrt((2 * this.randDrink.demandaAnual * this.randDrink.costoPedido) / this.randDrink.costoAlma);
-          this.resultado.cantidadOptima = Math.round(cpo * 1000) / 1000;
         }
-      },
-      calcularSigmaL() {
-        let v = Math.sqrt(this.modeloP.revision + this.randDrink.leadTime) * 1;
-        this.modeloP.sigmaL = Math.round(v * 1000) / 1000;
-      },
+      } else {
+        this.active = 0;
+      }
+      if (this.active === 2) {
+        if (this.isPSelected) {
+          this.calcularSigmaL();
+          this.calcularQ();
+        } else {
+          this.calcularReorden();
+          this.calcularCTE();
+          this.obtenerCantidadOrdenes();
+        }
+      }
+    },
+    preparar() {
+      this.randDrink.nivelServicio = this.randDrink.nivelServicio / 100;
+      if (this.randDrink.demandaAnual === 0) {
+        this.randDrink.demandaAnual =
+          this.randDrink.demandaDiaria * this.randDrink.diasLaborales * 12;
+      } else {
+        this.randDrink.demandaDiaria =
+          this.randDrink.demandaAnual / 12 / this.randDrink.diasLaborales;
+      }
+      this.calcularoL();
+      this.calcularQ();
+      this.calculoEz();
+    },
+    calcularoL() {
+      this.modeloQ.oL =
+        Math.sqrt(this.randDrink.leadTime) * this.randDrink.desviacionEstandar;
+    },
+    calcularReorden() {
+      let rta =
+        this.randDrink.demandaDiaria * this.randDrink.leadTime +
+        (this.modeloQ.eZ * this.modeloQ.oL);
+      this.resultado.reorden = rta;
+    },
+    calcularCTE() {
+      let cte =
+        this.randDrink.demandaAnual * this.randDrink.costo +
+        (this.randDrink.demandaAnual / this.resultado.cantidadOptima) *
+          this.randDrink.costoPedido +
+        (this.resultado.cantidadOptima / 2) * this.randDrink.costoAlma;
+      this.modeloQ.CTE = Math.round(cte * 1000) / 1000;
+    },
+    calculoEz() {
+      //((1-p)/q)/ol
+      let ez =
+        ((1 - this.randDrink.nivelServicio) * this.resultado.cantidadOptima) /
+        this.modeloQ.oL;
+      this.modeloQ.zD = Math.round(ez * 1000) / 1000;
+    },
+    calcularQ() {
+      if (this.isPSelected) {
+        let rta =
+          this.randDrink.demandaDiaria *
+            (this.modeloP.revision + this.randDrink.leadTime) +
+          this.randDrink.desviacionEstandar * this.modeloP.sigmaL -
+          this.modeloP.inventario;
+        if (rta < 0) {
+          rta = 0;
+        }
+        this.resultado.cantidadOptima = Math.floor(rta);
+      } else {
+        //si es q
+        let cpo = Math.sqrt(
+          (2 * this.randDrink.demandaAnual * this.randDrink.costoPedido) /
+            this.randDrink.costoAlma
+        );
+        this.resultado.cantidadOptima = Math.round(cpo * 1000) / 1000;
+      }
+    },
+    obtenerCantidadOrdenes(){
+      this.resultado.nOrdenes = Math.round(this.randDrink.demandaAnual / this.resultado.cantidadOptima);
+    },
+    calcularSigmaL() {
+      let v = Math.sqrt(this.modeloP.revision + this.randDrink.leadTime) * 1;
+      this.modeloP.sigmaL = Math.round(v * 1000) / 1000;
     }
   }
+};
 </script>
 <style>
-  .tableCalculos {
-    width: 90%;
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: 20px;
-  }
+.tableCalculos {
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 20px;
+}
 
+.continue {
+  margin-left: 70%;
+  display: inline;
+}
+
+.topBar {
+  margin-bottom: 15px;
+}
+
+.modelsHeader {
+  text-align: center;
+}
+
+@media only screen and (max-width: 800px) {
   .continue {
-    margin-left: 70%;
-    display: inline;
+    margin-left: 50%;
   }
-
-  .topBar {
-    margin-bottom: 15px;
-  }
-
-  .modelsHeader {
-    text-align: center;
-  }
-
-  @media only screen and (max-width:800px) {
-    .continue {
-      margin-left: 50%;
-    }
-  }
+}
 </style>
